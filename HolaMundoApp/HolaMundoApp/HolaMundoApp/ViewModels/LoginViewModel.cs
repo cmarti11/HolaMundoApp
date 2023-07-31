cc236a53db85448b198f92dcfa30471ee164319a
Cristian Martinez <107958139+cmartineznvc@users.noreply.github.com>
2023-07-31 10:18:56 -0500

    Add Images

--- HolaMundoApp/HolaMundoApp/HolaMundoApp/ViewModels/LoginViewModel.cs
+++ HolaMundoApp/HolaMundoApp/HolaMundoApp/ViewModels/LoginViewModel.cs
@@ -1,24 +1,39 @@
 ﻿using HolaMundoApp.Views;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
 
 namespace HolaMundoApp.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
-        public Command LoginCommand { get; }
-
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
         }
 
+        private string _username;
+        private string _password;
+
+        public string UserName { get => _username; set => SetProperty(ref _username, value); }
+        public string Password { get => _password; set => SetProperty(ref _password, value); }
+        public Command LoginCommand { get; }
+
+
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            if (ValidateFields())
+            {
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
         }
+        private bool ValidateFields()
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password))
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 }
